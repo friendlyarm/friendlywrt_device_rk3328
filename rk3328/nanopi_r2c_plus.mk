5214dfde85a5fb53674c327abd2dd8e0ac166b73
Lawrence-Tang <tangzongsheng@gmail.com>
2021-12-24 19:15:12 +0800

    add r2c plus

---
+++ rk3328/nanopi_r2c_plus.mk
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. device/friendlyelec/rk3328/base.mk
+TARGET_SD_RAW_FILENAME=FriendlyWrt_$(date +%Y%m%d)_NanoPi-R2C-Plus_arm64_sd.img
+TARGET_EFLASHER_RAW_FILENAME=FriendlyWrt_$(date +%Y%m%d)_NanoPi-R2C-Plus_arm64_eflasher.img
