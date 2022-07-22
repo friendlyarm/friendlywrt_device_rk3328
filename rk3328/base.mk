ecd9454d7ec3d6b8097fea3ca8af25cd0005e3a4
Lawrence-Tang <tangzongsheng@gmail.com>
2022-07-22 15:32:52 +0800

    add luci patch for factory reset function

--- rk3328/base.mk
+++ rk3328/base.mk
@@ -10,7 +10,7 @@ TARGET_OSNAME=friendlywrt
 
 TARGET_FRIENDLYWRT_CONFIG=rockchip
 FRIENDLYWRT_SRC=friendlywrt
-# FRIENDLYWRT_PATCHS+=(device/friendlyelec/rk3328/patches/0001-optimize-for-rk3328.patch)
+FRIENDLYWRT_PATCHS+=("feeds/luci/;device/common/src-patchs/feeds/luci/0001-fix-to-make-factory-reset-button-work-on-FriendlyWrt.patch")
 FRIENDLYWRT_FILES+=(device/common/ntfs3)
 FRIENDLYWRT_FILES+=(device/common/default-settings)
 FRIENDLYWRT_FILES+=(device/common/emmc-tools)
