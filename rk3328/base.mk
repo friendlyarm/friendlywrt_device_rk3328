5bd578c8df2e81e3bc889b439e4545e0b75c8794
Lawrence-Tang <tangzongsheng@gmail.com>
2022-09-16 19:01:43 +0800

    add friendlywrt.config

--- rk3328/base.mk
+++ rk3328/base.mk
@@ -12,9 +12,9 @@ TARGET_FRIENDLYWRT_CONFIG=rockchip
 FRIENDLYWRT_SRC=friendlywrt
 FRIENDLYWRT_PATCHS+=("feeds/luci/;device/common/src-patchs/21.02/feeds/luci")
 FRIENDLYWRT_FILES+=(device/common/ntfs3)
-FRIENDLYWRT_FILES+=(device/common/default-settings)
 FRIENDLYWRT_FILES+=(device/common/emmc-tools)
 FRIENDLYWRT_FILES+=(device/common/distfeeds)
+FRIENDLYWRT_FILES+=(device/common/default-settings)
 
 # U-boot
 # 
@@ -24,7 +24,7 @@ TARGET_UBOOT_CONFIG=nanopi_r2_defconfig
 # Kernel
 # 
 
-TARGET_KERNEL_CONFIG=nanopi-r2_linux_defconfig
+TARGET_KERNEL_CONFIG="nanopi-r2_linux_defconfig friendlywrt.config"
 TARGET_KERNEL_DTB=resource.img
 
 # Misc
