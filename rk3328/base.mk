7783d09a41869dd98523aadbb804b5326e893188
Lawrence-Tang <tangzongsheng@gmail.com>
2025-02-27 19:31:54 +0800

    add options to configure partitions: enable or disable opt partition, custom partition size etc.

--- rk3328/base.mk
+++ rk3328/base.mk
@@ -6,8 +6,7 @@ CROSS_COMPILER=aarch64-linux-gnu-
 TARGET_OSNAME=friendlywrt
 
 # FriendlyWRT
-# 
-
+#
 TARGET_FRIENDLYWRT_CONFIG=rockchip
 FRIENDLYWRT_SRC=friendlywrt
 FRIENDLYWRT_PATCHS+=("feeds/luci/;device/common/src-patchs/23.05/feeds/luci")
@@ -19,18 +18,24 @@ FRIENDLYWRT_FILES+=(device/common/distfeeds)
 FRIENDLYWRT_FILES+=(device/common/default-settings)
 
 # U-boot
-# 
-
+#
 TARGET_UBOOT_CONFIG=nanopi_r2_defconfig
 
 # Kernel
-# 
-
+#
 TARGET_KERNEL_CONFIG="nanopi-r2_linux_defconfig friendlywrt.config"
 TARGET_KERNEL_DTB=resource.img
 
 # Misc
-# 
+#
+ENABLE_OPT_PARTITION=true
+TARGET_ROOTFS_PARTSIZE=1024
+TARGET_USERDATA_PARTSIZE=1024
+if [ "${ENABLE_OPT_PARTITION}" = "true" ]; then
+    TARGET_SD_IMAGESIZE=3000
+else
+    TARGET_SD_IMAGESIZE=1500
+fi
 FRIENDLYWRT_ROOTFS=build_dir/target-aarch64_generic_musl/root-rockchip
 FRIENDLYWRT_PACKAGE_DIR=bin/targets/rockchip/armv8/packages
 TARGET_IMAGE_DIRNAME=friendlywrt23
